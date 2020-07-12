8d4a8062a22906b46fea2ffa648f44ea64232916
Benjamin Merrell <Benjamin.Merrell075@student.mckinneyisd.net>
2020-07-11 19:00:52 -0500

    small injector changes
    allow for me to test pyinstaller

--- src/app.py
+++ src/app.py
@@ -6,9 +6,9 @@ print('\nWelcome to the RocketCalculator!\n')
 print('1: Combustion Chamber \n2: Injector Design \n3: Appogee\n')
 calculator = input('Choose your calculator: \n')
 
-if calculator == '1':
+if calculator == 1: #make sure to change back to string
     c1.run()
-elif calculator == '2':
+elif calculator == 2: #make sure to change back to string
     c2.run()
 else :
     calculator = input('Choose a proper calculator: \n')
--- src/injector_nasa.py
+++ src/injector_nasa.py
@@ -21,22 +21,25 @@ def run():
     P1 = float(c)/At
     print(P1)
 
-    print('\nNext, we solve Bernouli\'s equation for the ratio of velocities') #TODO check necessity of this
+    print('\nNext, we solve for the ratio of velocities') #TODO check necessity of this
     print('\nFor this, we will need p (specific gravity) of the oxidizer and fuel, A (cross sectional area) of the fuel and oxidizer holes, and prefered k\'')
-    #pgf = input('\np of the fuel:') may not be needed
-    pgo = input('\np of the oxidizer:')
-    df = input('\ndiameter of the fuel outlet:') #TODO automate through ratios
-    do = input('\ndiameter of the oxidizer outlet:')
-    k = input('\nprefered k\'(recommended 0.625):')
+    pgf = input('\np of the fuel:\n') 
+    pgo = input('p of the oxidizer:\n')
+    df = input('diameter of the fuel outlet:\n') #TODO automate through ratios
+    do = input('diameter of the oxidizer outlet:\n')
+    k = input('prefered k\'(recommended 0.625):\n')
 
-    print('it is understood that v2 = 0, due to the liquids starting from rest at the valves')
-    rf = df/2
-    ro = do/2
-    af = pi*rf*rf
-    ao = pi*ro*ro
-    c1 = np.power(2*ao/af, 0.25)
-    v1 = np.sqrt(k/(c1*pgo))
-    print(v1)
+    rf = df/2 #radius of fuel outlet
+    ro = do/2 #radius of oxidizer outlet
+    af = pi*rf*rf #cross sectional area of fuel outlet
+    ao = pi*ro*ro #cross sectional area of oxidizer outlet
+    c1 = np.power(2*ao/af, 0.25) #part of formula
+    v1_v2 = np.sqrt(k/(c1*pgo)*pgf) #velocity ratios
+    print('\nv1/v2 ratio:')
+    print(v1_v2)
+
+    print('\nAfter that, we solve Bernoulli\'s equation for the estimated pressures') #may not need this either
+    
 
 
 
