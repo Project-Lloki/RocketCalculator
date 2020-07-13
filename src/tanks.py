6c569881ac315dc2995803c266f86b2a69f42603
Ismail Hozain <ismailhozain@gmail.com>
2020-07-13 12:50:42 -0500

    refactoring last commit.

--- src/tanks.py
+++ src/tanks.py
@@ -1,13 +1,9 @@
 # coding=utf-8
-import numpy as np
 import math
 
 def run():
     print("\nWelcome to the Tank Calculator! \n")
     print("Here you can find the volume of each tank including 1/3 ullage in liquid tanks (Vf), volume of each tank excluding 1/3 ullage (Vi), the inner radius of the tank(r), or the height of the tank (h) when you know at least three of these values.\n")
-    FU = 'N'
-    PU = ''
-    AU = ''
     print("\nChoose the value you wish to calculate for:")
     print("\n 1: Height from weight and radius. \n 2:Radius from Height and weight\n")
     solve = input("What are you solving for?\n")
@@ -38,13 +34,10 @@ def run():
             volume = 1.5 * (weight / keroseneWeightPerCubedFoot)
             return format(math.sqrt((volume/(math.pi*h))), '0.3f') + " feet or " + format(math.sqrt((volume/(math.pi*h))) * 12, '0.3f') + " inches"
 
-
-
-
     if(solve == '1'):
         print(height())
     elif(solve == '2'):
         print(radius())
     else :
-        print("Invalid input, use 1, 2, 3 or 4 + ENTER")
+        print("Invalid input, use 1 or 2 + ENTER")
 
