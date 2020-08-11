3d288fe4c82a00955963e881065591430e8269de
Benjamin Merrell <Benjamin.Merrell075@student.mckinneyisd.net>
2020-08-10 21:59:09 -0500

    add units to excel, improve units.py

--- src/excel.py
+++ src/excel.py
@@ -1,59 +1,38 @@
 import xlwt
 from xlwt import Workbook
-import arial10
 
 wb = Workbook()
 used = False
 
 
-class FitSheetWrapper(object):
-    """Try to fit columns to max size of any entry.
-    To use, wrap this around a worksheet returned from the 
-    workbook's add_sheet method, like follows:
-
-        sheet = FitSheetWrapper(book.add_sheet(sheet_name))
-
-    The worksheet interface remains the same: this is a drop-in wrapper
-    for auto-sizing columns.
-    """
-
-    def __init__(self, sheet):
-        self.sheet = sheet
-        self.widths = dict()
-
-    def write(self, r, c, label='', *args, **kwargs):
-        self.sheet.write(r, c, label, *args, **kwargs)
-        width = arial10.fitwidth(label)
-        if width > self.widths.get(c, 0):
-            self.widths[c] = width
-            self.sheet.col(c).width = width
-
-    def __getattr__(self, attr):
-        return getattr(self.sheet, attr)
-
-
-def sheet(vars, names, eqs, s_name):
+def sheet(vars, names, units, eqs, s_name):
     global used
     used = True
-    sheet1 = FitSheetWrapper(wb.add_sheet(s_name))
+    sheet1 = wb.add_sheet(s_name)
 
     style = xlwt.easyxf('font: bold 1')
 
     sheet1.write(0, 0, 'Variables', style)
     sheet1.write(0, 1, 'Values', style)
-    sheet1.write(0, 2, 'Method', style)
+    sheet1.write(0, 2, 'Units', style)
+    sheet1.write(0, 3, 'Method', style)
 
     for y in range(0, len(names)):
         sheet1.write((y+1), 0, names[y])
     for x in range(0, len(vars)):
         sheet1.write((x+1), 1, vars[x])
+    for w in range(0, len(units)):
+        sheet1.write((w+1), 2, units[w])
     for z in range(0, len(eqs)):
-        sheet1.write((z+1), 2, eqs[z])
+        sheet1.write((z+1), 3, eqs[z])
 
 
 def pretty(strs):
-    for x in strs:
-        x.replace('**', '^')
-        x.replace('np.', '')
+    new = []
 
-    return strs
+    for x in strs:
+        x = x.replace('**', '^')
+        x = x.replace('np.', '')
+        new.append(x)
+
+    return new
--- src/units.py
+++ src/units.py
@@ -1,23 +1,33 @@
-system = FU = PU = AU = VU = MdU = TU = g = DU = None
+system = FU = PU = AU = VU = vU = VoU = Mo = ang = aU = MdU = TU = g = DU = None
 
 
 def system():
-    global system, FU, PU, AU, VU, MdU, TU, g, DU
+    global system
     print("1: Metric 2: Imperial\n")
     system = input("What units do you use: ")
-    if(system == '1'):
-        FU = 'N'
-        PU = ''
-        AU = ''
-        g = 9.81
-    elif(system == '2'):
-        FU = 'Lbf'
-        PU = 'psi'
-        AU = 'in^2'
-        VU = 'ft/sec'
-        MdU = 'Lbm/sec'
-        TU = 'R°'
-        g = 32.2
-        DU = 'in'
-    else:
-        print("Invalid input:")
+
+    def set_units():
+        global FU, PU, AU, VU, vU, VoU, Mo, ang, MdU, aU, TU, g, DU
+        if(system == '1'):
+            FU = 'N'
+            PU = ''
+            AU = ''
+            g = 9.81
+        elif(system == '2'):
+            FU = 'Lbf'
+            PU = 'psi'
+            AU = 'in^2'
+            VU = 'ft^3/lb'
+            VoU = 'ft^3'
+            vU = 'ft/sec'
+            MdU = 'Lbm/sec'
+            TU = '°R'
+            Mo = 'lb/mole'
+            g = 32.2
+            DU = 'in'
+            ang = '°'
+            aU = 'ft/sec'
+        else:
+            print("Invalid input!\n")
+            set_units()
+    set_units()
