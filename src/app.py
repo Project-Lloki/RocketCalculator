08f750332360a37f673f94df5e9abe8003f7cd17
Ben Merrell <benjamin.merrell075@student.mckinneyisd.net>
2021-01-15 16:39:07 -0600

    work on helium.pu

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ import cc as c1
 import injector as c2
 import tanks as c3
 import apogee2 as c4
+import helium as c5
 import units as u
 
 unstarted = True
@@ -14,7 +15,7 @@ def run():
 
     u.init()
 
-    print('1: Combustion Chamber \n2: Injector Design \n3: Tanks\n4: Apogee Optimization')
+    print('1: Combustion Chamber \n2: Injector Design \n3: Tanks\n4: Apogee Optimization\n4: Helium Optimization')
     calculator = input('\nChoose your calculator: ')
 
     if calculator == '1':
@@ -25,6 +26,8 @@ def run():
         c3.run()
     elif calculator == '4':
         c4.run()
+    elif calculator == '5':
+        c5.run()
     else:
         print('choose a proper calculator')
 
