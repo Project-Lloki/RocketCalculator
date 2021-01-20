885df7e1b6fd6d7d92bc8f1bccb8c58dd231b92a
Ben Merrell <benjamin.merrell075@student.mckinneyisd.net>
2021-01-19 19:24:03 -0600

    add thrustfile.py

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import cc as c1
 import injector as c2
 import tanks as c3
+import thrustFile as c5
 import units as u
 
 unstarted = True
@@ -13,7 +14,7 @@ def run():
 
     u.init()
 
-    print('1: Combustion Chamber \n2: Injector Design \n3: Tanks\n4: Apogee Optimization')
+    print('1: Combustion Chamber \n2: Injector Design \n3: Tanks\n4: Apogee Optimization \n5: Thrust File')
     calculator = input('\nChoose your calculator: ')
 
     if calculator == '1':
@@ -22,6 +23,8 @@ def run():
         c2.run()
     elif calculator == '3':
         c3.run()
+    elif calculator == '5':
+        c5.run()
     else:
         print('choose a proper calculator')
 
